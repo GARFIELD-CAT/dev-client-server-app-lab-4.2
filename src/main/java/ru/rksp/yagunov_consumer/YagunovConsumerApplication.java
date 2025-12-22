906ba251fce44b3e4b02338a10ac2d7bc823dedd
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-22 22:33:29 +0300

    add init consumer service

---
+++ src/main/java/ru/rksp/yagunov_consumer/YagunovConsumerApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.yagunov_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagunovConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagunovConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/yagunov_consumer/YagunovConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.yagunov_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagunovConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
