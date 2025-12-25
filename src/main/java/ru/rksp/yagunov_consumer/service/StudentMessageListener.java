b2623d36b9cc06c3243ae8aff0cf4221748b9647
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-25 17:18:48 +0300

    add logs

--- src/main/java/ru/rksp/yagunov_consumer/service/StudentMessageListener.java
+++ src/main/java/ru/rksp/yagunov_consumer/service/StudentMessageListener.java
@@ -1,12 +1,14 @@
 package ru.rksp.yagunov_consumer.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import ru.rksp.yagunov_consumer.model.StudentMessage;
 import ru.rksp.yagunov_consumer.model.StudentMessageEntity;
 import ru.rksp.yagunov_consumer.repository.StudentMessageRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StudentMessageListener {
@@ -15,12 +17,14 @@ public class StudentMessageListener {
 
     @RabbitListener(queues = "${app.rabbitmq.queue}")
     public void handleStudentMessage(StudentMessage message) {
+        log.info("------------Сообщение успешно получено------------");
         StudentMessageEntity entity = new StudentMessageEntity();
         entity.setFullName(message.getFullName());
         entity.setPassport(message.getPassport());
         entity.setCreatedAt(message.getCreatedAt());
 
         repository.save(entity);
+        log.info("------------Сообщение успешно обработано------------");
     }
 }
 
